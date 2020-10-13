f70b826da2844b8e0c175b6e34163cf2210d3fcf
repl.it user <replituser@example.com>
2020-10-13 11:22:01 +0000

    code demonstrates use of user difeined function with parameter

---
+++ basics/functions/function_with_parameter.py
@@ -0,0 +1,12 @@
+def escape_by(plan):
+  if plan==str("jumping over"):
+    print("We cannot escape that way! The boulder is too big!")
+  elif plan==str("running around"):
+    print("We cannot escape that way! The boulder is moving too fast!")
+  elif plan==str("going deeper"):
+    print("That might just work! Let's go deeper!")
+  else:
+    print("We cannot escape that way! The boulder is in the way!")
+escape_by("jumping over")
+escape_by("running around")
+escape_by("going deeper") 
