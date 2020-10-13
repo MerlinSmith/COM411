1c4d57fc1593c1b9e69281c88a0f65dd7d381e38
repl.it user <replituser@example.com>
2020-10-13 10:55:49 +0000

    code demonstrates user defined func with a nested decision

---
+++ basics/functions/function_with_nesting.py
@@ -0,0 +1,12 @@
+#define see function
+def identify():
+  #ask user what they See
+  print("what do you see?")
+  see=input()
+  boulder=str("a large boulder")
+  if(see==boulder):
+    print("it's time to run")
+  else:
+    print("we will be fine")
+identify()
+
