b66d3bbe4bf2c5038c4dd953c3a1ae427096bf9b
repl.it user <replituser@example.com>
2020-10-27 16:01:55 +0000

    code shows simple tuple

--- data/tuples/simple_tuple.py
+++ data/tuples/simple_tuple.py
@@ -1,3 +1,10 @@
 def likelihood():
   likelihoods = (50, 38, 27, 99, 4)
   return min(likelihoods)
+
+def run():
+  likelihood() 
+  minLike = likelihood() 
+  print("Minimum likelihood of falling: {}%".format(minLike))
+
+run()
