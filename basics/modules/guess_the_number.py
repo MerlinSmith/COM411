e8ebdc86429410af121ce969c6da29291ce99928
Merlin Smith <4smitm94@solent.ac.uk>
2020-11-06 22:08:28 +0000

    updated guess the number to be contained in a function

--- basics/modules/guess_the_number.py
+++ basics/modules/guess_the_number.py
@@ -1,22 +1,24 @@
+def play_guess_the_number():
+  print("Please enter the minimum value:") 
+  min = int(input())
+  print("Please enter the maximum value:")
+  max = int(input())
+  print("I am thinking of a number between {} and {}.  Can you guess what it is?".format(min, max))
+  import random
+  random_number = random.randrange(min, max)
+  guess = 0
 
-print("Please enter the minimum value:") 
-min = int(input())
-print("Please enter the maximum value:")
-max = int(input())
-print("I am thinking of a number between {} and {}.  Can you guess what it is?".format(min, max))
-import random
-random_number = random.randrange(min, max)
-guess = 0
+  while (guess != random_number):
+    guess=int(input())
 
-while (guess != random_number):
-  guess=int(input())
+    if(guess == random_number):
+      print("congrats!")
+      break
+    elif(guess < random_number):
+      print("guess higher")
+    elif(guess > random_number):
+      print("guess lower")
 
-  if(guess == random_number):
-    print("congrats!")
-    break
-  elif(guess < random_number):
-    print("guess higher")
-  elif(guess > random_number):
-    print("guess lower")
-
-print("Game Over")
+  print("Game Over")
+  
+play_guess_the_number()
