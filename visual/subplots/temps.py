4638597a009117e15ca09dba009973cfeac3eed4
Merlin Smith <4smitm94@solent.ac.uk>
2020-11-20 13:57:17 +0000

    renamed subplots to temps.py

---
+++ visual/subplots/temps.py
@@ -0,0 +1,35 @@
+import csv
+import matplotlib.pyplot as plt
+first_key = None
+second_key = None
+def read_data():
+  global first_key, second_key
+  with open("visual/subplots/temps.csv") as csvfile:
+      csv_reader = csv.reader(csvfile)
+      header = next(csv_reader) #fetches next row
+      first_key = header[0].strip()
+      second_key = header[1].strip()
+      data = {
+         first_key:[],
+        second_key:[]
+      }
+      for row in csv_reader:
+        data[first_key].append(row[0].strip())
+        data[second_key].append(row[1].strip())
+      
+      return data
+
+def run():
+  data = read_data()
+  
+  fig, (ax1, ax2) = plt.subplots(2, 1, sharex = 'all')
+  ax1.plot(data[first_key])
+  ax2.plot(data[second_key])
+  plt.show()
+run()
+
+    
+ 
+
+    
+    
